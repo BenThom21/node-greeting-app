1c1ab4caa704137bbce2847a468e87905bdde434
Ben <benthompson4496@gmail.com>
2018-10-03 06:36:01 -0500

    forgot git init - first commit

---
+++ greeting.js
@@ -0,0 +1,19 @@
+// module.exports = {
+//     greetingEnglish: function() {
+//         console.log("Hello");
+//     },
+
+//     greetingJapanese: function() {
+//         console.log("Konichiwa");
+//     }
+// };
+
+exports.greetingEnglish = function(){
+    console.log("Hello");
+}
+  
+exports.greetingJapanese = function(){
+    console.log("Konichiwa");
+}
+
+exports = "Hola";
